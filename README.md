0e937121bd6d685eac39c119cf59db3295ce1538
marius <marius@coretrek.no>
2025-02-05 15:25:16 +0100

    feat: update services

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Docker environment for my projects
+## Basic LAMP Stack
 
 ### Includes:
-- PHP 8.1
+- PHP 8.2.27
 - MySQL 8.0
 - PHPMyAdmin
--- sql/init.sql
+++
@@ -1,6 +0,0 @@
-USE blizzlikecms;
-
-CREATE TABLE `users` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, 'demo', 'demo', 'demo@demo.com');
-
-CREATE TABLE `forum_posts` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `content` VARCHAR(255) NOT NULL , `author_id` INT(11) NOT NULL , `date` BIGINT(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
